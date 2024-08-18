3d1fafbbfa8c4d6c7c1a4d0cdc4f4daaece33ba9
kyllianlucas <kyllianlucas0@gmail.com>
2024-08-18 12:18:09 +0200

    Update main.jsx

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './header.jsx'
+import App from './Header'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
